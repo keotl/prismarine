0e52777da71092d37c5070bc7ae1c1ac178d77cf
Kento A. Lauzon <kento.lauzon@ligature.ca>
2018-09-20 10:56:02 -0400

    fix server unable to start from .deb

--- build-dpkg.sh
+++ build-dpkg.sh
@@ -4,7 +4,7 @@ mkdir build
 git clone https://github.com/keotl/prismarine-ui.git build/ui
 cd build/ui && npm install && npm run build && cd ../..
 
-mkdir -p build/prismarine/debian build/prismarine/usr/lib/prismarine-server/static build/prismarine/etc/nginx/sites-available build/prismarine/etc/systemd/user
+mkdir -p build/prismarine/debian build/prismarine/usr/lib/prismarine-server/static build/prismarine/etc/nginx/sites-available build/prismarine/etc/systemd/system
 cp -r build/ui/dist/* build/prismarine/usr/lib/prismarine-server/static
 cp -r prismarine build/prismarine/usr/lib/prismarine-server
 cp main.py build/prismarine/usr/lib/prismarine-server
@@ -14,7 +14,7 @@ cp requirements.txt build/prismarine/usr/lib/prismarine-server
 cp -r deploy/debian build/prismarine
 
 cp deploy/config/nginx.conf build/prismarine/etc/nginx/sites-available/prismarine
-cp deploy/config/prismarine.service build/prismarine/etc/systemd/user/
+cp deploy/config/prismarine.service build/prismarine/etc/systemd/system/
 
 #TODO remove, directory structure should be created correctly from the start
 mv build/prismarine/debian build/prismarine/DEBIAN
